78eafea40d71017adb1b81df42f7eb7f03df918c
Aqurius <AquriusHnin@gmail.com>
2017-05-15 15:57:43 +0630

    new

--- resources/views/frontend/rooms.blade.php
+++ resources/views/frontend/rooms.blade.php
@@ -13,7 +13,7 @@
 
 	
 		<div class="col-sm-6 wowload fadeInUp">
-			<div class="rooms"><img src="{{ asset($roomtype->image) }}" class="img-responsive">
+			<div class="rooms"><img src="{{ asset('img/frontendrooms/'.$roomtype->image) }}" class="img-responsive">
 				<div class="info"><h3>{{  $roomtype->name }}&nbsp&nbsp ${{  $roomtype->price }}</h3><p> {{  $roomtype->features }}</p>
 				<i class="fa fa-phone" aria-hidden="true" style="font-size:25px;color:#585650;"></i> &nbsp&nbsp&nbsp
 				<i class="fa fa-wifi" aria-hidden="true" style="font-size:25px;color:#585650;"></i> &nbsp&nbsp&nbsp
